0bbb253271ce21b52ed8b30f8b4246f25152dc60
Sachin P K <Sanjaypk585@gmail.com>
2026-03-22 18:43:54 +0530

    Move admin branding overrides to apps.py ready method

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        from django.contrib import admin
+        admin.site.site_header = "DonateHub Administration"
+        admin.site.site_title = "DonateHub Admin Portal"
+        admin.site.index_title = "Welcome to DonateHub Administration"
